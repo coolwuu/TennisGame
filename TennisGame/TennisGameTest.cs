233413d2237488a7ed8f89be1b3e8597c2dae4ec
coolwuu <coolwuu0207@gmail.com>
2017-11-03 00:37:40 +0800

    [R] remove duplicate code

--- TennisGame/TennisGameTest.cs
+++ TennisGame/TennisGameTest.cs
@@ -10,32 +10,35 @@ namespace TennisGame
 
     public class TennisGameTest
     {
-       
+        private Game TennisGame;
+
         [Test]
         public void StartGame_Score_should_be_LoveAll()
         {
-            Player a = new Player { Name = "a" };
-            Player b = new Player { Name = "b" };
-            Game tennisGame = new Game(a, b);
-            //act
-            var actual = tennisGame.ShowScore();
+            var tennisGame = InitialGame();
 
-            //assert
+            var actual = tennisGame.ShowScore();
             Assert.AreEqual("Love All", actual);
         }
 
         [Test]
         public void A_Get1stPoint_Score_should_be_Fifteen_All()
         {
-            Player a = new Player { Name = "a" };
-            Player b = new Player { Name = "b" };
-            Game tennisGame = new Game(a, b);
-            //act
+            var tennisGame = InitialGame();
+
             tennisGame.FirstPlayerScore();
             var actual = tennisGame.ShowScore();
 
             //assert
             Assert.AreEqual("Fifteen Love", actual);
+        }
+
+        private static Game InitialGame()
+        {
+            Player a = new Player { Name = "a" };
+            Player b = new Player { Name = "b" };
+            Game tennisGame = new Game(a, b);
+            return tennisGame;
         }
     }
 }
