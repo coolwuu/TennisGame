5c30905d9d08f1332dd161815d46296c08af1cd5
coolwuu <coolwuu0207@gmail.com>
2017-11-03 01:16:47 +0800

    pass test

--- TennisGame/Game.cs
+++ TennisGame/Game.cs
@@ -16,10 +16,27 @@ namespace TennisGame
         {
             if (_firstPlayer.ScoringTimes == 0 && _secondPlayer.ScoringTimes == 0)
                 return "Love All";
-            if (_firstPlayer.ScoringTimes >= 3 && _secondPlayer.ScoringTimes >= 3 &&
-                _firstPlayer.ScoringTimes == _secondPlayer.ScoringTimes)
-                return "Deuce";
+            if (_firstPlayer.ScoringTimes >= 3 && _secondPlayer.ScoringTimes >= 3)
+            {
+                if (_firstPlayer.ScoringTimes == _secondPlayer.ScoringTimes)
+                {
+                    return "Deuce";
+                }
+                return LateGameLogic();
+            }
             return _firstPlayer.Score() + " " + _secondPlayer.Score();
+        }
+
+        private string LateGameLogic()
+        {
+            if (_firstPlayer.ScoringTimes - _secondPlayer.ScoringTimes == 1)
+            {
+                return "Advantage " + _firstPlayer.Name;
+            }
+            else
+            {
+                return "Advantage " + _secondPlayer.Name;
+            }
         }
 
         public void FirstPlayerWinAPoint()
